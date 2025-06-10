29c47336ffe63470db5273b8297c09e1f696a617
priyanshu rawat <priyanshu-r001@webreinvent.com>
2025-06-10 12:55:16 +0530

    used user factory to seed, added AdminSeeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -27,6 +27,8 @@ class UserFactory extends Factory
             'id' => Str::uuid(),
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
+            'role' => fake()->randomElement(['student', 'employer', 'employee', 'teacher']),
+            'status' => fake()->randomElement(['pending', 'approved']),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,15 +13,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory(10)->unverified()->create();
 
         // User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
 
-        $this->call([
-            UserSeeder::class
-        ]);
+        // $this->call([
+        //     //
+        // ]);
     }
 }
